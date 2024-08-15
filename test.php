2e2f96d4c7dc686b4fe74162e1af3e28e9f83c21
im-ushan-ikshana <ikushan23261@gmail.com>
2024-08-15 17:30:01 +0530

    Initial commit: project upload

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    fucfjkejekj
+</body>
+</html>
