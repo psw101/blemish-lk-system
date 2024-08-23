d329f29967e69935821f0e4897885a1b2a7ed3c4
Hirushan Subasinghe <117560218+Hirushan-Subasinghe@users.noreply.github.com>
2024-08-23 15:03:13 +0530

    refactor: Remove unused search form in navbar.php
    Co-authored-by: Tharushi<rathnay-im21111@stu.kln.ac.lk>

--- includes/navbar.php
+++ includes/navbar.php
@@ -120,7 +120,7 @@
          </button>
 
          <!-- Topbar Search -->
-         <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+         <!-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
            <div class="input-group">
              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
              <div class="input-group-append">
@@ -129,11 +129,8 @@
                </button>
              </div>
            </div>
-         </form>
-
-
-         <!-- Topbar Navbar -->
-         <ul class="navbar-nav ml-auto">
+         </form> -->
+        <h5 class="ml-3 mt-2 p-2" style="color: #8d4741;">Blemish: Where Perfection Meets Style...</h5>       <!-- Topbar Navbar -->       <ul class="navbar-nav ml-auto">
 
            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
            <li class="nav-item dropdown no-arrow d-sm-none">
