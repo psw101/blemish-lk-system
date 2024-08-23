673925c531b21315560183e1200b5e66e3b77ee1
Hirushan Subasinghe <117560218+Hirushan-Subasinghe@users.noreply.github.com>
2024-08-23 10:06:25 +0530

    refactor: Update table column names in inventory.php

--- inventory.php
+++ inventory.php
@@ -46,21 +46,21 @@ include('includes/navbar.php');
                             <tr>
                                 <th scope="col">Product ID</th>
                                 <th scope="col">Product Name</th>
-                                <th scope="col">Quantity</th>
+                                <th scope="col">Quantity In Stock</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
-                            $fetch_query = "SELECT * FROM supplier";
+                            $fetch_query = "SELECT * FROM inventory";
                             $fetch_query_run = mysqli_query($con, $fetch_query);
 
                             if (mysqli_num_rows($fetch_query_run) > 0) {
                                 while ($row = mysqli_fetch_array($fetch_query_run)) {
                             ?>
                                     <tr>
-                                        <td class="user_id"><?php echo $row['id']; ?></td>
-                                        <td><?php echo $row['name']; ?></td>
-                                        <td><?php echo $row['email']; ?></td>
+                                        <td class="user_id"><?php echo $row['product_id']; ?></td>
+                                        <td><?php echo $row['product_name']; ?></td>
+                                        <td><?php echo $row['quantity_in_stock']; ?></td>
                                         
                                     </tr>
                                 <?php
@@ -91,108 +91,3 @@ include('includes/scripts.php');
 include('includes/footer.php');
 ?>
 
-<script>
-    //view data start
-    $(document).ready(function() {
-        $('.view_data').click(function(e) {
-            e.preventDefault();
-
-
-            var user_id = $(this).closest('tr').find('.user_id').text();
-            // console.log(user_id);
-
-            $.ajax({
-                method: "POST",
-                url: "supplier.php",
-                data: {
-                    'click_view_btn': true,
-                    'user_id': user_id,
-                },
-                success: function(response) {
-                    // console.log(response);
-
-                    $('.view_user_data').html(response);
-                    $('#viewuser').modal('show');
-
-                }
-            });
-
-
-        })
-    });
-    //view data end
-
-
-    // Edit data start
-    $(document).ready(function() {
-        $('.edit_data').click(function(e) {
-            e.preventDefault();
-
-
-            var user_id = $(this).closest('tr').find('.user_id').text();
-            // console.log(user_id);
-
-            $.ajax({
-                method: "POST",
-                url: "supplier.php",
-                data: {
-                    'click_edit_btn': true,
-                    'user_id': user_id,
-                },
-                success: function(response) {
-                    // console.log(response);
-
-                    $.each(response, function(key, value) {
-                        // console.log(value['name']);
-                        $('#supplier_id').val(value['id']);
-                        $('#name').val(value['name']);
-                        $('#email').val(value['email']);
-                        $('#phone').val(value['phone']);
-                        $('#address').val(value['address']);
-                        //id,name,email,.. are database column names.    user_id,name,email,.. are form's field ids used in modal
-
-                    });
-
-
-                    $('#editdata').modal('show');
-
-                }
-            });
-
-
-        })
-    });
-    // Edit data end
-
-    //Delete data start
-    $(document).ready(function() {
-        $('.delete_btn').click(function(e) {
-            e.preventDefault();
-
-
-            var user_id = $(this).closest('tr').find('.user_id').text();
-            // console.log(supplier_id);
-
-            $.ajax({
-                method: "POST",
-                url: "supplier.php",
-                data: {
-                    'click_delete_btn': true,
-                    'user_id': user_id,
-                },
-                success: function(response) {
-                    console.log(response);
-                    window.location.reload();
-
-                    // $('.view_user_data').html(response);
-                    // $('#viewuser').modal('show');
-
-                }
-            });
-
-
-
-        })
-    });
-    //Delete data end
-</script>
