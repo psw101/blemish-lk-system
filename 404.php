09f68e2eac1a5cc747114612cee513f9d288f108
im-ushan-ikshana <ikushan23261@gmail.com>
2024-08-25 01:31:35 +0530

    refactor: Add 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+include_once('./includes/header.php');
+include_once('./includes/navbar.php');
+?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- 404 Error Text -->
+  <div class="text-center">
+    <div class="error mx-auto" data-text="404">404</div>
+    <p class="lead text-gray-800 mb-5">Page Not Found</p>
+    <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+    <a href="index.php">&larr; Back to Dashboard</a>
+  </div>
+
+</div>
+<!-- /.container-fluid -->
+
+<?php
+include_once('./includes/footer.php');
+include_once('./includes/scripts.php');
+?>
