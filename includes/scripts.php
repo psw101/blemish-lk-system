cf4c1c4cff54a88ae142638caea650fddfb1bf90
im-ushan-ikshana <ikushan23261@gmail.com>
2024-08-25 20:50:11 +0530

    refactor: Simplify codebase by removing unused files and codes.

--- includes/scripts.php
+++ includes/scripts.php
@@ -97,35 +97,3 @@
         $('#alertModal').modal('show');
     }
 </script>
-
-<?php
-
-include_once('./dbcon.php');
-
-if (isset($_POST['registerbtn'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirmpassword'];
-
-    if ($password === $confirm_password) {
-        $query = "INSERT INTO register (username,email,password) VALUES ('$username','$email','$password')";
-        $query_run = mysqli_query($con, $query);
-
-        if ($query_run) {
-            echo "done";
-            $_SESSION['success'] =  "Admin is Added Successfully";
-            header('Location: register.php');
-        } else {
-            echo "not done";
-            $_SESSION['status'] =  "Admin is Not Added";
-            header('Location: register.php');
-        }
-    } else {
-        echo "pass no match";
-        $_SESSION['status'] =  "Password and Confirm Password Does not Match";
-        header('Location: register.php');
-    }
-}
-
-?>
