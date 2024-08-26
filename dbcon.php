8d9d015dc59b590d4f89215736478a30b7f87e52
Ushan Ikshana <89129852+im-ushan-ikshana@users.noreply.github.com>
2024-08-26 22:45:29 +0530

    refactor: Update database connection configuration in dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,5 @@
 <?php 
-    $con = mysqli_connect("localhost","root","23261!UshanIkshana","blemish_inventory");
+    $con = mysqli_connect("localhost","root","","blemish_inventory");
 
     if(!$con){
         die("Connection Failed :" . mysqli_connect_error());
